c89901e5bb35ea2033d967e06cead65ef9fef0aa
Zink77 <72149937+Zink77@users.noreply.github.com>
2020-09-30 10:54:51 -0500

    Add files via upload

---
+++ NumberGame.java
@@ -0,0 +1,52 @@
+//import the needed utilities
+import java.util.Scanner;
+import java.util.Random;
+
+//begin main class
+public class NumberGame {
+    public static void main(String[] args)
+    {
+        //sets up the utilities
+        Scanner keyboard = new Scanner(System.in);
+        Random rand = new Random();
+
+        //create variables
+        int secretNumber = rand.nextInt(101) ;
+        int playerGuess;
+        int guessAttempts = 1;
+        boolean correct = false;
+
+        //get your first guess
+        System.out.println("Please guess a number between 1-100: ");
+        playerGuess = keyboard.nextInt();
+
+        //begins the game loop
+        while (!correct)
+        {
+            //check if guess is too low
+            if (playerGuess < secretNumber)
+            {
+                System.out.println(playerGuess + " is too low, please guess again: ");
+                playerGuess = keyboard.nextInt();
+                guessAttempts += 1;
+            }
+
+            //check if guess is too high
+            if (playerGuess > secretNumber)
+            {
+                System.out.println(playerGuess + " is too high, please guess again: ");
+                playerGuess = keyboard.nextInt();
+                guessAttempts += 1;
+            }
+
+            //checks if guess is correct
+            if (playerGuess == secretNumber)
+            {
+                correct = true;
+            }
+        }
+
+        //prints the win statement and guess number
+        System.out.println(playerGuess + " was the correct answer! It only took you " + guessAttempts + " guesses!");
+    }
+}
